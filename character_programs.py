d22e58f758f7edfcc3549b4d421a7565542e1524
omkar <omkarsp20@gmail.com>
2026-01-03 22:02:35 +0530

    Added Python Basic String Characters Solutions

---
+++ character_programs.py
@@ -0,0 +1,77 @@
+# Python program to check Alphabet or not
+'''
+char = input("Enter any character :- ")
+if(char >= 'a' and char <= 'z') or (char >= 'A' and char <= 'Z'):
+    print(f"{char} is an Alphabet")
+else:
+    print(f"{char} is not an Alphabet")
+'''
+
+# Python program to check Alphabet or Digit
+'''
+char = input("Enter any character :- ")
+if(char >= 'a' and char <= 'z') or (char >= 'A' and char <= 'Z'):
+    print(f"{char} is an Alphabet")
+elif(char >= '0' and char <= '9'):
+    print(f"{char} is Digit")
+else:
+    print(f"{char} is not digit or alphabet)
+'''
+
+# Python program to check Character is an Alphabet, Digit or Special Character
+'''
+char = input("Enter any character :- ")
+if(char >= 'a' and char <= 'z') or (char >= 'A' and char <= 'Z'):
+    print(f"{char} is an Alphabet")
+elif(char >= '0' and char <= '9'):
+    print(f"{char} is Digit")
+else:
+    print(f"{char} is special symbol")
+'''
+
+# Python program to check Digit or Not
+'''
+char = input("Enter any character :- ")
+if(char >= '0' and char <= '9'):
+    print(f"{char} is Digit")
+else:
+    print(f"{char} is not a Digit")
+'''
+
+# Python program to check Lowercase or not
+'''
+char = input("Enter any character :- ")
+if(char.islower()):
+    print(f"{char} is in Lowercase")
+else:
+    print(f"{char} is not in Lowercase")
+'''
+
+# Python program to check Lowercase or Uppercase
+'''
+char = input("Enter any character :- ")
+if(char.islower()):
+    print(f"{char} is in Lowercase")
+elif(char.isupper()):
+    print(f"{char} is in Uppercase")
+else:
+    print(f"{char} not both")
+'''
+
+# Python Program to check Uppercase or not
+'''
+char = input("Enter any character :- ")
+if(char.isupper()):
+    print(f"{char} is in Uppercase")
+else:
+    print(f"{char} is not in Uppercase")
+'''
+
+# Python program to check Vowel or Consonant
+'''
+char = input("Enter any characyer :- ")
+if(char in "aeiouAEIOU"):
+    print(f"{char} is Vowel")
+else:
+    print(f"{char} is Consonant")
+'''
